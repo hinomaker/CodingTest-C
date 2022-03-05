56d2dd630ef15ce839f1cf492d4ee844f10acd0d
Sangmin Park <35327930+SHDPR@users.noreply.github.com>
2022-03-05 14:35:51 +0900

    2022-03-05

---
+++ 상민/0305/파싱.cpp
@@ -0,0 +1,31 @@
+#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+int main(){
+  string curr = "first:second third fourth";
+  vector<string> lst;
+
+  int start = 0;
+  int delim = 0;
+
+  for(; delim <= curr.size(); delim++)
+  {
+    /*
+    delim == curr.size()인 경우,
+    curr[delim] 접근 시 에러가 나기 때문에
+    이 조건을 가장 앞에 놓아 체크해야 합니다!
+    */
+    if(delim == curr.size() || curr[delim] == ':' || curr[delim] == ' ')
+    {
+      lst.push_back(curr.substr(start, delim - start));
+      start = delim + 1;
+    }
+  }
+
+  for(int idx = 0; idx < lst.size(); idx++){
+    cout << lst[idx] << endl;
+  }
+}
